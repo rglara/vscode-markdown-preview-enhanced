6837116925b9b1e0a79d33f3c8ad259c8b35405b
Greg Lara <rglara@users.noreply.github.com>
2019-03-18 22:19:59 -0400

    write test to check configuration enums

--- test/extension.test.js
+++ test/extension.test.js
@@ -5,19 +5,50 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
+const extensionName = "markdown-preview-enhanced";
+const fullExtensionName = "shd101wyy." + extensionName;
+
 // The module 'assert' provides assertion methods from node
-var assert = require("assert");
+const assert = require("assert");
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-var vscode = require("vscode");
-var myExtension = require("../out/src/extension");
+const vscode = require("vscode");
+//const myExtension = require("../out/src/extension");
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
-  // Defines a Mocha unit test
-  test("Something 1", function() {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+suite("Configuration Tests", () => {
+  test("Ensure default enum settings are within enum set", () => {
+    const ext = vscode.extensions.getExtension(fullExtensionName);
+    assert.ok(ext, "Couldn't get extension: " + fullExtensionName);
+    assert.ok(ext.packageJSON, "Unexpected extension JSON");
+    assert.ok(ext.packageJSON.contributes, "Unexpected contributes JSON");
+    assert.ok(
+      ext.packageJSON.contributes.configuration,
+      "Unexpected configuration JSON",
+    );
+    const configSettings = ext.packageJSON.contributes.configuration.properties;
+    assert.ok(configSettings, "Missing properties JSON");
+    const keys = Object.keys(configSettings);
+    for (let i = 0; i < keys.length; ++i) {
+      const key = keys[i];
+      const setting = configSettings[key];
+      if (setting.hasOwnProperty("enum")) {
+        assert.ok(
+          Array.isArray(setting.enum),
+          "Setting enum should be array for " + key,
+        );
+        const defaultValue = setting.default;
+        assert.ok(defaultValue, "Default value not set for " + key);
+        assert.ok(
+          setting.enum.includes(defaultValue),
+          "Default value '" +
+            defaultValue +
+            "' is not contained in enum list for '" +
+            key +
+            "'",
+        );
+      }
+    }
   });
 });
