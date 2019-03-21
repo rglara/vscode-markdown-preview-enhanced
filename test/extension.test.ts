9d8daec65d9a3ea91a6062634044b540d0d9861f
Greg Lara <rglara@users.noreply.github.com>
2019-03-20 21:50:32 -0400

    convert test files to typescript
    (typescript tests make linter/formatter happy)

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,9 @@
-/* global suite, test */
-
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
+import * as assert from "assert";
+import * as vscode from "vscode";
 
 const extensionName = "markdown-preview-enhanced";
 const fullExtensionName = "shd101wyy." + extensionName;
 
-// The module 'assert' provides assertion methods from node
-const assert = require("assert");
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-const vscode = require("vscode");
-//const myExtension = require("../out/src/extension");
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Configuration Tests", () => {
   test("Ensure default enum settings are within enum set", () => {
     const ext = vscode.extensions.getExtension(fullExtensionName);
@@ -25,7 +12,7 @@ suite("Configuration Tests", () => {
     assert.ok(ext.packageJSON.contributes, "Unexpected contributes JSON");
     assert.ok(
       ext.packageJSON.contributes.configuration,
-      "Unexpected configuration JSON"
+      "Unexpected configuration JSON",
     );
     const configSettings = ext.packageJSON.contributes.configuration.properties;
     assert.ok(configSettings, "Missing properties JSON");
@@ -36,17 +23,17 @@ suite("Configuration Tests", () => {
       if (setting.hasOwnProperty("enum")) {
         assert.ok(
           Array.isArray(setting.enum),
-          "Setting enum should be array for " + key
+          "Setting enum should be array for " + key,
         );
         const defaultValue = setting.default;
         assert.ok(defaultValue, "Default value not set for " + key);
         assert.ok(
           setting.enum.includes(defaultValue),
           "Default value '" +
-          defaultValue +
-          "' is not contained in enum list for '" +
-          key +
-          "'"
+            defaultValue +
+            "' is not contained in enum list for '" +
+            key +
+            "'",
         );
       }
     }
